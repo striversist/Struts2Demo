b416805f62f954d2389ce813a3a12809e28c56e9
striversist <striversist@qq.com>
2016-02-27 11:04:46 +0800

    [annotation]注解的使用用例。

---
+++ src/demo/actions/HelloWorld2Action.java
@@ -0,0 +1,35 @@
+package demo.actions;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.InterceptorRef;
+import org.apache.struts2.convention.annotation.InterceptorRefs;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Namespaces;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Namespaces(value={@Namespace("/demo"), @Namespace("/")})
+@Action(value="hello2", results={
+	@Result(name="success", location="/HelloWorld.jsp"),
+	@Result(name="error", location="/AccessDenied.jsp")
+})
+@InterceptorRefs(value={@InterceptorRef("params"), @InterceptorRef("timer")})
+public class HelloWorld2Action extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	private String name;
+    
+    @Override
+    public String execute() throws Exception {
+    	name = "HelloWorld2Action";
+        return SUCCESS;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
