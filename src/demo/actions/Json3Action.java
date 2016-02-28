5a52eee5e4a5a30ff25ea9c33fe019fe7c534ff1
striversist <striversist@qq.com>
2016-02-28 11:26:56 +0800

    [json]方式3 返回json数据：使用struts2-json-plugin（“json-default”，“type root”，“params root”，“map的getter方法”） “

---
+++ src/demo/actions/Json3Action.java
@@ -0,0 +1,33 @@
+package demo.actions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@ParentPackage("json-default")
+public class Json3Action extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+	// 这里指定将被Struts2序列化的属性，该属性在action中必须有对应的getter方法
+	private Map<String, Object> dataMap = new HashMap<>();
+
+	@Action(value="json3", results={@Result(type="json", params={"root", "dataMap"})})
+	public String doJson3() throws Exception {
+		dataMap.clear();
+		dataMap.put("result", 0);
+		dataMap.put("id", "123");
+		dataMap.put("name", "aaron");
+		dataMap.put("password", "pwd123");
+		dataMap.put("msg", "hello world from json3");
+		return SUCCESS;
+	}
+	
+	public Map<String, Object> getDataMap() {
+		return dataMap;
+	}
+}
