54ebff2380dc415da80305975189e01d6dfdb060
aarontang <aarontang@tencent.com>
2016-02-29 14:25:19 +0800

    [download]使用struts2的方式实现文件下载

---
+++ src/demo/actions/DownloadAction.java
@@ -0,0 +1,39 @@
+package demo.actions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DownloadAction extends ActionSupport {
+
+    private static final long serialVersionUID = 1L;
+    private static final String FILE_PATH = "G:\\Projects\\J2EE\\Struts2Demo\\downloads\\light.jpg";
+    private InputStream fileInputStream;
+    private String downloadFileName;
+
+    @Action(value="download", results={
+        @Result(name="success", type="stream", params={"contentType", "application/octet-stream",
+                "inputName", "fileInputStream", "contentDisposition", "attachment;filename=${downloadFileName}",
+                "bufferSize", "4096"})
+    })
+    public String download() throws Exception {
+        File downloadFile = new File(FILE_PATH);
+        fileInputStream = new FileInputStream(downloadFile);
+        downloadFileName = downloadFile.getName();
+        
+        return SUCCESS;
+    }
+    
+    public InputStream getFileInputStream() {
+        return fileInputStream;
+    }
+    
+    public String getDownloadFileName() {
+        return downloadFileName;
+    }
+}
